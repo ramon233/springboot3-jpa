6ae5471111da824cbe50a55cae24e0853f66e2a3
Ramon p <rp499963@gmail.com>
2025-07-01 11:59:51 -0300

    first commit

---
+++ src/test/java/com/rweb/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
