9b9dd98dacc901c57310ec918d934ae992323750
Ramon p <rp499963@gmail.com>
2025-07-07 22:53:47 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/rweb/demo/entities/User.java
+++ src/main/java/com/rweb/demo/entities/User.java
@@ -23,7 +23,7 @@ public class User implements Serializable{
 	
 	public User() {}
 
-	public User(long id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
